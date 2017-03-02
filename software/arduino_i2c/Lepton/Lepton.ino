4a7a044daa9a52201b712ab47deede1558e683e8
Ian Glen Neal <ian.gl.neal@gmail.com>
2017-03-02 01:07:58 -0600

    Lots of edits.

--- software/arduino_i2c/Lepton/Lepton.ino
+++ software/arduino_i2c/Lepton/Lepton.ino
@@ -28,7 +28,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <SPI.h>
 
 byte x = 0;
-#define ADDRESS  (0x2A)
+#define ADDRESS (0x2A)
 
 #define AGC (0x01)
 #define SYS (0x02)
@@ -41,80 +41,136 @@ byte x = 0;
 
 #define VOSPI_FRAME_SIZE (164)
 
-void setup()
-{
+SPISettings flir_settings(25000000, MSBFIRST, SPI_MODE3);
+#define CS_PIN 10
+
+void setup() {
   //pinMode(pin, INPUT);           // set pin to input
   //digitalWrite(pin, HIGH);
 
   Wire.begin();
   Serial.begin(115200);
+  while(!Serial); // wait for the Serial monitor
 
-  pinMode(10, OUTPUT);
-  SPI.setDataMode(SPI_MODE3);
-  SPI.setClockDivider(0);
+  pinMode(CS_PIN, OUTPUT);
+  //SPI.setDataMode(SPI_MODE3);
+  //SPI.setClockDivider(0);
 
   SPI.begin();
 
   Serial.println("setup complete");
 }
 
-int spi_read_word(int data)
-{
+void scan_for_i2c() {
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    Wire.write("01234567890123456789");
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.print(address, HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    } else if (error <= 4) {
+      Serial.print("Unknown error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.print(address,HEX);
+      Serial.print(" error = ");
+      Serial.println(error, HEX);
+    } else {
+      Serial.print("Bytes = ");
+      Serial.println(error);
+    }
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  } else {
+    Serial.println("done\n");
+  }
+}
+
+int spi_read_word(int data) {
   int read_data;
-  // take the SS pin low to select the chip:
-  digitalWrite(10, LOW);
-  //  send in the address and value via SPI:
+  SPI.beginTransaction(flir_settings);
+  // take the CS pin low to select the chip:
+  digitalWrite(CS_PIN, LOW);
+  // send in the address and value via SPI:
   read_data = SPI.transfer(data >> 8) << 8;
   read_data |= SPI.transfer(data);
-  // take the SS pin high to de-select the chip:
-  digitalWrite(10, HIGH);
+  // take the CS pin high to de-select the chip:
+  digitalWrite(CS_PIN, HIGH);
+  SPI.endTransaction();
   return read_data;
 }
 
-byte lepton_frame_packet[VOSPI_FRAME_SIZE];
-
 #define IMAGE_SIZE (800)
+byte lepton_frame_packet[VOSPI_FRAME_SIZE];
 byte image[IMAGE_SIZE];
 int image_index;
-void read_lepton_frame(void)
-{
-  int i;
-  for (i = 0; i < (VOSPI_FRAME_SIZE / 2); i++)
-  {
-    //digitalWrite(10, LOW);
-    PORTB &= ~(1 << 2);
+
+void read_lepton_frame(void) {
+  for (int i = 0; i < (VOSPI_FRAME_SIZE / 2); i++) {
+    SPI.beginTransaction(flir_settings);
+    digitalWrite(CS_PIN, LOW);
+    //PORTB &= ~(1 << 2);
     //  send in the address and value via SPI:
     lepton_frame_packet[2 * i] = SPI.transfer(0x00);
     lepton_frame_packet[2 * i + 1] = SPI.transfer(0x00);
 
-
     // take the SS pin high to de-select the chip:
-    //digitalWrite(10, HIGH);
-    PORTB |= 1 << 2;
+    digitalWrite(10, HIGH);
+    SPI.endTransaction();
+
+    //PORTB |= 1 << 2;
   }
 }
 
-void lepton_sync(void)
-{
-  int i;
+void lepton_sync(void) {
   int data = 0x0f;
 
-  PORTB |= 1 << 2;
+  SPI.beginTransaction(flir_settings);
+  digitalWrite(CS_PIN, HIGH);
+  SPI.endTransaction();
+  //PORTB |= 1 << 2;
   delay(185);
-  while (data & 0x0f == 0x0f) {
-    PORTB &= ~(1 << 2);
+  while ((data & 0x0f) == 0x0f) {
+    SPI.beginTransaction(flir_settings);
+    digitalWrite(CS_PIN, LOW);
+    //PORTB &= ~(1 << 2);
     data = SPI.transfer(0x00) << 8;
     data |= SPI.transfer(0x00);
-    PORTB |= 1 << 2;
+    digitalWrite(CS_PIN, HIGH);
+    SPI.endTransaction();
+    //PORTB |= 1 << 2;
 
-    for (i = 0; i < ((VOSPI_FRAME_SIZE - 2) / 2); i++) {
+    for (int i = 0; i < ((VOSPI_FRAME_SIZE - 2) / 2); i++) {
 
-      PORTB &= ~(1 << 2);
+      SPI.beginTransaction(flir_settings);
+      digitalWrite(CS_PIN, LOW);
+      //PORTB &= ~(1 << 2);
 
       SPI.transfer(0x00);
       SPI.transfer(0x00);
 
-      PORTB |= 1 << 2;
+      digitalWrite(CS_PIN, HIGH);
+      SPI.endTransaction();
+      //PORTB |= 1 << 2;
     }
   }
 
@@ -125,25 +181,21 @@ void print_lepton_frame(void) {
   Serial.print("S");
   for (int i = 0; i < (VOSPI_FRAME_SIZE); i++) {
     Serial.print(lepton_frame_packet[i], HEX);
-    Serial.print(",");
+    //Serial.print(",");
   }
-  Serial.print("E");
+  Serial.print("X");
 }
 
-void print_image(void)
-{
-  int i;
-  for (i = 0; i < (IMAGE_SIZE); i++)
-  {
+void print_image(void) {
+  for (int i = 0; i < (IMAGE_SIZE); i++) {
     Serial.print(image[i], HEX);
     Serial.print(",");
-
   }
   Serial.println(" ");
 }
 
 void lepton_command(unsigned int moduleID, unsigned int commandID,
-                    unsigned int command)c{
+                    unsigned int command) {
   byte error;
   Wire.beginTransmission(ADDRESS);
 
@@ -151,19 +203,16 @@ void lepton_command(unsigned int moduleID, unsigned int commandID,
   Wire.write(0x00);
   Wire.write(0x04);
 
-  if (moduleID == 0x08) //OEM module ID
-  {
+  //OEM module ID
+  if (moduleID == 0x08) {
     Wire.write(0x48);
-  }
-  else
-  {
+  } else {
     Wire.write(moduleID & 0x0f);
   }
   Wire.write( ((commandID << 2 ) & 0xfc) | (command & 0x3));
 
   error = Wire.endTransmission();    // stop transmitting
-  if (error != 0)
-  {
+  if (error <= 4) {
     Serial.print("error=");
     Serial.println(error);
   }
@@ -187,7 +236,7 @@ void agc_enable() {
 void set_reg(unsigned int reg)
 {
   byte error;
-  Wire.beginTransmission(ADDRESS); // transmit to device #4
+  Wire.beginTransmission(ADDRESS);   // transmit to device #4
   Wire.write(reg >> 8 & 0xff);
   Wire.write(reg & 0xff);            // sends one byte
 
@@ -221,46 +270,51 @@ int read_reg(unsigned int reg)
   return reading;
 }
 
-int read_data()
-{
-  int i;
-  int data;
-  int payload_length;
-
-  while (read_reg(0x2) & 0x01)
-  {
+void read_data() {
+  while (read_reg(0x2) & 0x01) {
     Serial.println("busy");
+    delay(1000);
   }
 
-  payload_length = read_reg(0x6);
+  int payload_length = read_reg(0x6);
   Serial.print("payload_length=");
   Serial.println(payload_length);
 
   Wire.requestFrom(ADDRESS, payload_length);
   //set_reg(0x08);
-  for (i = 0; i < (payload_length / 2); i++)
-  {
-    data = Wire.read() << 8;
+  for (int i = 0; i < (payload_length / 2); i++) {
+    int data = Wire.read() << 8;
     data |= Wire.read();
     Serial.println(data, HEX);
   }
-
 }
 
 
-void loop()
-{
-  int i;
-  int reading = 0;
+void loop() {
+  //int i;
+  //int reading = 0;
   String debugString;
+
+  // Just wait for the user to hit enter.
+  Serial.println("Press enter to continue.");
+  while(Serial.available() == 0);
+  Serial.read();
+
   Serial.println("beginTransmission");
 
-  //set_reg(0);
+  /*
+  while(1) {
+    scan_for_i2c();
+    delay(5000);
+  }
 
-  //read_reg(0x0);
+  set_reg(0);
+
+  read_reg(0x0);
+
+  while(1);
 
   read_reg(0x2);
-
 
   Serial.println("SYS Camera Customer Serial Number");
   lepton_command(SYS, 0x28 >> 2 , GET);
@@ -306,11 +360,14 @@ void loop()
   // Serial.println("SYS Telemetry Enable State");
   //lepton_command(SYS, 0x19>>2 ,GET);
   // read_data();
+  */
 
-  while (1)
-  {
+  while (1) {
     //lepton_sync();
     read_lepton_frame();
+    print_lepton_frame();
+    Serial.println();
+    delay(5000);
     /*
     if(lepton_frame_packet[i]&0x0f != 0x0f ) {
       print_lepton_frame();
